eaa703f86bcd436d9065cfb539f95bfb99d3338a
Fabian Daquilema <109368214+Kirbo97@users.noreply.github.com>
2024-09-15 14:45:59 -0500

    Add files via upload

--- JS/funciones.js
+++ JS/funciones.js
@@ -16,23 +16,31 @@ function cerrarDialog(tip) {
         
 // Función para mostrar el contenido adecuado según la procesadora seleccionada
 function mostrarContenido(tipo) {
-    // Ocultar todos los contenidos primero
-    var contenidos = document.querySelectorAll('.ocultar');
-        contenidos.forEach(function(contenido) {
-            contenido.style.display = 'none';
-        });
-                
-    let partes = tipo.split(","); // creo un arreglo a partir de la frase (cada caracter tendra su propia celda) para cambiar las casillas requeridas
-        
-    // Mostrar el contenido correspondiente al tipo de procesadora
-    if (partes[0] === 'normal') {
-        document.querySelectorAll('.contenidoNormal').forEach(function(contenido) { contenido.style.display = 'block'; });
-        document.querySelectorAll('.cantidadNormal').forEach(function(cantidad) { cantidad.style.display = 'block'; });
-    } else if (partes[0] === 'gigante') {
-        document.querySelectorAll('.contenidoGigante').forEach(function(contenido) { contenido.style.display = 'block'; });
-        document.querySelectorAll('.cantidadGigante').forEach(function(cantidad) { cantidad.style.display = 'block'; });
-    }
 
-    var dialog = document.getElementById(partes[1]);
+    var valido = tipo.includes(',');
+
+    var dialog;
+        
+    if (valido === true) {
+        // Ocultar todos los contenidos primero
+        var contenidos = document.querySelectorAll('.ocultar');
+    
+            contenidos.forEach(function(contenido) { contenido.style.display = 'none'; });
+            
+        let partes = tipo.split(","); // creo un arreglo a partir de la frase (cada caracter tendra su propia celda) para cambiar las casillas requeridas
+            
+        // Mostrar el contenido correspondiente al tipo de procesadora
+        if (partes[0] === 'normal') {
+            document.querySelectorAll('.contenidoNormal').forEach(function(contenido) { contenido.style.display = 'block'; });
+            document.querySelectorAll('.cantidadNormal').forEach(function(cantidad) { cantidad.style.display = 'block'; });
+        } else if (partes[0] === 'gigante') {
+            document.querySelectorAll('.contenidoGigante').forEach(function(contenido) { contenido.style.display = 'block'; });
+            document.querySelectorAll('.cantidadGigante').forEach(function(cantidad) { cantidad.style.display = 'block'; });
+        }
+    
+        dialog = document.getElementById(partes[1]);
+        
+    }// else { dialog = document.getElementById(tipo); }
+
     dialog.showModal();
 }            
